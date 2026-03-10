e942e0299245f2a55097780cd7b8926eb6ecb39c
AnshumanShah69 <februaryleapyear29@gmail.com>
2026-03-10 08:10:46 +0530

    updated settings.py

---
+++ src/Tools/__init__.py
@@ -0,0 +1,4 @@
+"""Tools module for Agentic rag - For exporting"""
+
+# from .lookup_rag import 
+# check if MCP implementation required
--- src/crew/crewmanager.py
+++ src/crew/crewmanager.py
@@ -6,14 +6,19 @@ from crewai import Agent, Crew, Task, LLM
 from langchain_google_genai import ChatGoogleGenerativeAI
 from crewai.memory import ShortTermMemory, LongTermMemory
 from src.configuration.settings import settings
+from typing import Dict
+from pathlib import Path
 
 load_dotenv()
 
 class CrewManager:
     def __init__(self, agent_config_path: str):
+        ##current directory parent folder path
+        self.crew_dir = Path(__file__).parent
         self.agent_config_path = agent_config_path
+        self.listed_mcp_tools = 
         self.llm = self.initialize_llm()
-        self.agents = self.listed_agents()
+        self.agents = self.boot_agents()
         self.tasks = self.boot_tasks()
         ##Private to prevent the memory() call from other files and functions
         self._conversation_memory()
@@ -21,7 +26,10 @@ class CrewManager:
         # def _conversation_memory(self):
         #     try:
         #         ###Conditional Memory flag check###
-        #         if settings.
+        #         if settings.short_term_memory_flag or settings.long_term_memory_flag:
+
+        #     except Exception as e:
+        #         print(f"Error occured as :{e}")
 
     def initialize_llm(self):
         try:
@@ -36,3 +44,21 @@ class CrewManager:
         except Exception as e:
             print(f'Failed to initialize the llm with issue as: {e}')
             raise
+
+    def boot_agents(self) -> Dict:
+        """Loading agents for cortexsail.ai"""
+        try: 
+            agent_configuration = self.crew_dir / "agents.yaml"
+            with open(agent_configuration,'r', encoding='utf-8') as f:
+                ##safeload parsers yaml -> return python object
+                agent_configuration = yaml.safe_load(f)
+                ##temp storage var for agents
+                agents{}
+
+                mcp_tools = list(self.listed_mcp_tools)
+                ###Initialize the tools in Tools folder then enable
+
+
+                # mcp_tools.append(search_similar_defect)
+
+                
